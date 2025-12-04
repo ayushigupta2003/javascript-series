b3e7ab59c5c0f27e26f0ca6fd0702c0064b35ce3
Ayushi Gupta <your-ayushigga@gmail.com>
2025-12-04 23:15:27 +0530

    basics

---
+++ basic_js/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 289878
+let accountEmail ="ayushi@gamil.com"
+var accountPasswor ="12345"
+accountcity ="indor"
+
+let  accountState = 9999;
+
+
+// accountId = 2
+accountEmail = "shdsj"
+accountPasswor = "222222"
+accountcity="bhooooopali"
+
+/*
+prefer not use var
+beacause of issue in block scope anf functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPasswor,accountcity,accountState])
---
+++ basic_js/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(9+4) //e are using node.js not browser
+
+// console.log(3+
+//     3); //code readabbility  should bhe high
+
+
+ // console.log(22)
+
+ //jvascript have 2 datatyes 
+
+// primitive data types -> nnbbssu
+ 
+//nmmber => 2 to poer 53
+//null  => standalone value   console.log(typeof null) -> object data type hai
+//boolean => true / false
+//bigint => 888999999
+//string =>""
+//symbol=>unique ness
+//undefind  =>console.log(typeof undefined) // undefind he atat hai
+
+
+
+
+//non-primitive data types
+
